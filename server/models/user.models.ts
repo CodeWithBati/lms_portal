8af757513690e85dea8963138124a85cc18c9324
CodeWithBati <xyaan.bati@gmail.com>
2024-03-23 17:16:02 +0500

    UserModel complete

--- server/models/user.models.ts
+++ server/models/user.models.ts
@@ -17,42 +17,65 @@ export interface IUser extends Document {
   comparePassowrd: (password: string) => Promise<boolean>;
 }
 
-const userSchema: Schema<IUser> = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter your name"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please enter your email"],
-    validate: {
-      validator: function (value: string) {
-        return emailRegexPattren.test(value);
+const userSchema: Schema<IUser> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattren.test(value);
+        },
+        message: "Please enter a valid email",
       },
-      message: "Please enter a valid email",
     },
-  },
-  password: {
-    type: String,
-    required: [true, "please enter your password"],
-    minlength: [8, "Password should be 8 characters"],
-    select: false,
-  },
-  avatar: {
-    public_is: String,
-    url: String,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-  isVerified: {
-    type: Boolean,
-    default: false,
-  },
-  courses: [
-    {
-      courseId: String,
+    password: {
+      type: String,
+      required: [true, "please enter your password"],
+      minlength: [8, "Password should be 8 characters"],
+      select: false,
     },
-  ],
+    avatar: {
+      public_is: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+//hash password before saving
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
+
+//compare password
+userSchema.methods.comparePassowrd = async function (
+  enteredPassword: string
+): Promise<Boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//Exporting userModel
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+export default userModel;
