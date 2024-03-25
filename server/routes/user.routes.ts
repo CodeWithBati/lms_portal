f6f93952eca737e7c9586dde2ad4f1841544bd8b
CodeWithBati <xyaan.bati@gmail.com>
2024-03-26 00:33:13 +0500

    update access token

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import Express from "express";
-import { activateUser, LoginUser, LogOut, registrationUser } from "../controllers/user.controller";
+import { activateUser, LoginUser, LogOut, registrationUser, UpdateAccessToken } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = Express.Router();
 
@@ -7,5 +7,6 @@ userRouter.post("/registration", registrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", LoginUser);
 userRouter.get("/logout", isAuthenticated, LogOut);
+userRouter.get("/refresh", UpdateAccessToken);
 
 export default userRouter;
