aafb1ca9d3b6f2a183da235f8f476f4cc5dc4d48
CodeWithBati <xyaan.bati@gmail.com>
2024-03-26 22:55:18 +0500

    Social Auth

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import Express from "express";
-import { activateUser, LoginUser, LogOut, registrationUser, UpdateAccessToken } from "../controllers/user.controller";
+import { activateUser, getUserInfo, LoginUser, LogOut, registrationUser, socialAuth, UpdateAccessToken } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
 const userRouter = Express.Router();
 
@@ -8,5 +8,7 @@ userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", LoginUser);
 userRouter.get("/logout", isAuthenticated, LogOut);
 userRouter.get("/refresh", UpdateAccessToken);
+userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/socialauth", socialAuth)
 
 export default userRouter;
