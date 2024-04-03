5873b38b0afc9729a260cd5134a6b8bfc31610ff
CodeWithBati <xyaan.bati@gmail.com>
2024-04-04 01:06:35 +0500

    Edit Course

--- server/routes/course.routes.ts
+++ server/routes/course.routes.ts
@@ -1,9 +1,10 @@
 import Express from "express";
 const courseRouter = Express.Router();
 import { authoriseRole, isAuthenticated } from "../middleware/auth";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 
 courseRouter.post("/create-course", isAuthenticated, authoriseRole("admin"), uploadCourse);
+courseRouter.put("/edit-course/:id", isAuthenticated, authoriseRole("admin"), editCourse);
 
 
 export default courseRouter;
